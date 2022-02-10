817248057de83e0be0f8c9a15dd894885d746b4a
BolaTEC <76160871+BolaTEC@users.noreply.github.com>
2022-02-10 14:53:37 +0100

    Fix comments

--- HUZZAH_http_client_to_php/arduino_secrets.h
+++ HUZZAH_http_client_to_php/arduino_secrets.h
@@ -1,4 +1,3 @@
-#define SECRET_SSID "<Dit netværks SSID>"    // TEC hvis du er på skolen
-#define SECRET_USER "<Unilogin@elev.tec.dk>" // Hvis du er på skolen
+#define SECRET_SSID "<Dit netværks SSID>"
 #define SECRET_PASS "<Password/key til netværket>"
 #define SECRET_SERVER "http://<Få info fra læreren>"
